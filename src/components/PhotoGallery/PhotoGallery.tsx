ac390d4f5b6358a62351c7a3c9a602a016bdaa48
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-12-23 17:58:05 -0700

    refactor: remove  styles component PhotoGallery  and GalleryPage

--- src/components/PhotoGallery/PhotoGallery.tsx
+++ src/components/PhotoGallery/PhotoGallery.tsx
@@ -1,27 +1,27 @@
-import { PicturesContainer, PicturesHeader } from "./PhotoGallery.styles";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-// import Loading from "../Loading";
 import HoverButton from "../HoverButton/HoverButton";
 import { ButtonMode } from "../HoverButton/HoverButton.styles";
-import * as S from "../../pageLayouts/GalleryPage.styles";
+import { motion } from "framer-motion";
 
 interface PhotoProps {
   picUrls: string[];
+  className?: string;
 }
 const PhotoGallery = ({ picUrls }: PhotoProps) => {
   return (
     <>
-      <PicturesHeader
+      <motion.div
         animate={{ opacity: 1 }}
+        className="min-h-screen"
         initial={{ opacity: 0 }}
         transition={{ delay: 0.5 }}
       >
-        <S.GalleryPageHeader>
-          <p>
+        <div className="text-center">
+          <p className="sm-max:text-regular-font text-babyHeading-size text-white">
             Capturing the Memories: A Look Inside Tech Start! <br /> Interested
             in joining us?
           </p>
-          <div>
+          <div className="mt-[3vw] flex justify-center">
             <HoverButton
               glowOnHover={true}
               link="/apply"
@@ -30,9 +30,9 @@ const PhotoGallery = ({ picUrls }: PhotoProps) => {
               text={"Apply Now"}
             />
           </div>
-        </S.GalleryPageHeader>
+        </div>
         {picUrls.length > 0 && (
-          <PicturesContainer>
+          <div className="mx-auto my-8 max-w-5xl columns-1 gap-6 sm:columns-2 lg:columns-3">
             {picUrls.map((photo: string, index: number) => (
               <LazyLoadImage
                 height="auto"
@@ -41,9 +41,9 @@ const PhotoGallery = ({ picUrls }: PhotoProps) => {
                 width="100%"
               />
             ))}
-          </PicturesContainer>
+          </div>
         )}
-      </PicturesHeader>
+      </motion.div>
     </>
   );
 };
