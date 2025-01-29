3f43727af6f3df293560aad9be2d0b9907302935
30178597 <sultan.amrani@ucalgary.ca>
2025-01-28 18:40:04 -0700

    v1.01 .map of merchCards, dlt old components

--- src/pages/merch/MerchSection/MerchData.ts
+++ src/pages/merch/MerchSection/MerchData.ts
@@ -1,30 +1,20 @@
-export type MerchProperties = {
-  id: number;
-  item: string;
-  price: string;
-  image: string;
-  description: string;
-  sizes: string[];
-  colors: string[];
-};
 
-export const merchList: MerchProperties[] = [
+
+
+
+export const MerchItemsData = [
   {
-    colors: ["Black"],
-    description: "placeholder descripton",
     id: 0,
-    image: "/image/merch/BasicCrewneck.jpg",
-    item: "Original Basic Crewneck",
-    price: "$29.95 CAD",
-    sizes: ["S", "M", "L", "XL"],
+    title: "Classic Crew Neck",
+    cloathinImg: "/image/merch/BasicCrewneck.jpg",
+    price: "29.95",
+    link: "https://docs.google.com/forms/d/e/1FAIpQLSfpXS4hisen7IBvMGZnrfYWH600W_vpJwW0-b7blsA-D5Dq2w/viewform"
   },
   {
-    colors: ["Black"],
-    description: "placeholder descripton",
     id: 1,
-    image: "/image/merch/BasicCrewneckWithSleevePrint.jpg",
-    item: "Crewneck with Sleeve Print",
-    price: "$49.99 CAD",
-    sizes: ["S", "M", "L", "XL"],
-  },
-];
+    title: "Crew Neck with Custom Sleeve Print",
+    cloathinImg: "/image/merch/BasicCrewneckWithSleevePrint.jpg",
+    price: "49.99",
+    link: "https://docs.google.com/forms/d/e/1FAIpQLSfpXS4hisen7IBvMGZnrfYWH600W_vpJwW0-b7blsA-D5Dq2w/viewform"
+  }
+]
