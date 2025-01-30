c2174af9e647699d10333cce1e142ce88e6ee3a1
30178597 <sultan.amrani@ucalgary.ca>
2025-01-30 13:51:22 -0700

    v1.03 fixed recommede changes but new with path bug?

--- src/lib/data/MerchData.ts
+++ src/lib/data/MerchData.ts
@@ -1,13 +1,21 @@
-export const MerchItemsData = [
+type MerchItem = {
+  clothingImg: string; // Corrected property name
+  id: number;
+  link: string;
+  price: string;
+  title: string;
+};
+
+export const MerchItemsData: MerchItem[] = [
   {
-    cloathinImg: "/image/merch/BasicCrewneck.jpg",
+    clothingImg: "/image/merch/BasicCrewneck.jpg",
     id: 0,
     link: "https://docs.google.com/forms/d/e/1FAIpQLSfpXS4hisen7IBvMGZnrfYWH600W_vpJwW0-b7blsA-D5Dq2w/viewform",
     price: "29.95",
     title: "Classic Crew Neck",
   },
   {
-    cloathinImg: "/image/merch/BasicCrewneckWithSleevePrint.jpg",
+    clothingImg: "/image/merch/BasicCrewneckWithSleevePrint.jpg",
     id: 1,
     link: "https://docs.google.com/forms/d/e/1FAIpQLSfpXS4hisen7IBvMGZnrfYWH600W_vpJwW0-b7blsA-D5Dq2w/viewform",
     price: "49.99",
