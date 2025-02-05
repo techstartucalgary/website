bf5a0685bc35985faa47ee9a628eb30458fbed07
Sukritib13 <113867416+Sukritib13@users.noreply.github.com>
2025-02-05 02:00:54 -0700

    icon placement and font fix

--- src/components/TeamSection/Profile.tsx
+++ src/components/TeamSection/Profile.tsx
@@ -37,12 +37,12 @@ const Profile: React.FC<ProfileProps> = ({
 }) => {
   return (
     <div
-      className="mx-[1%] mb-10 flex w-[clamp(150px,20%,300px)] flex-col items-center sm:w-60 md:w-72 lg:w-80"
+      className="relative mx-[1%] mb-10 flex w-[clamp(150px,20%,300px)] flex-col items-center sm:w-60 md:w-72 lg:w-80"
       data-aos={!mobileView ? "zoom-in" : ""}
       data-aos-duration={!mobileView ? "1000" : ""}
     >
       {profilePic && (
-        <div className="flex w-full justify-center">
+        <div className="relative flex w-full justify-center">
           <Image
             alt={alt}
             className="mt-[15px] aspect-square w-full rounded-full bg-white object-cover"
@@ -51,18 +51,18 @@ const Profile: React.FC<ProfileProps> = ({
             src={profilePic}
             width={150}
           />
-        </div>
-      )}
 
-      {member.linkedin && (
-        <div
-          className={`absolute bottom-0 right-0 flex items-center justify-center ${getBackgroundColor(activeCategory)} size-[60px] rounded-full sm:size-[70px] md:size-[70px] lg:size-[80px]`}
-        >
-          <SocialMedia
-            color="White"
-            icon={faLinkedinIn}
-            link={member.linkedin}
-          />
+          {member.linkedin && (
+            <div
+              className={`absolute right-[15%] top-[60%] flex translate-x-1/2 translate-y-1/2 items-center justify-center ${getBackgroundColor(activeCategory)} size-[75px] rounded-full sm:size-[80px] md:size-[90px] lg:size-[100px]`}
+            >
+              <SocialMedia
+                color="White"
+                icon={faLinkedinIn}
+                link={member.linkedin}
+              />
+            </div>
+          )}
         </div>
       )}
 
--- src/components/TeamSection/ProfileDescription.tsx
+++ src/components/TeamSection/ProfileDescription.tsx
@@ -5,9 +5,21 @@ interface ProfileDescriptionProps {
 
 const ProfileDescription = ({ affiliation, name }: ProfileDescriptionProps) => {
   return (
-    <div className="mt-[20%] text-center font-[Inter]">
-      <h3 className="m-0 text-[1.1rem] font-black text-white">{name}</h3>
-      <h4 className="m-0 text-[0.95rem] text-white">{affiliation}</h4>
+    <div className="mt-[20%] text-center">
+      <h3
+        className="m-0 mx-2 text-[2.0rem] font-black tracking-wide text-white"
+        style={{
+          fontFamily: "Inter, Tahoma, sans-serif",
+        }}
+      >
+        {name}
+      </h3>
+      <h4
+        className="m-0 mx-2 mt-4 text-[1.5rem] text-white"
+        style={{ fontFamily: "Inter, Tahoma, sans-serif" }}
+      >
+        {affiliation}
+      </h4>
     </div>
   );
 };
