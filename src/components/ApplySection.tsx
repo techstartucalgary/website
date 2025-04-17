7e6e9bdcf3d2c986f04b51d02ce2bf59efda4d78
Sahiti Akella <63627104+Sahitiakella@users.noreply.github.com>
2025-04-17 17:44:42 -0600

    feat: migrate apply page (#29)
    * migrated apply page
    
    * inline tailwind css
    
    * resolved issue with exec image not found
    
    * fix spacing between footer and apply buttons
    
    * yarn version changes
    
    * addressing comments
    
    * formatting changes
    
    ---------
    
    Co-authored-by: s-akella04 <sakella@cestatech.com>
    Co-authored-by: IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>

--- src/components/ApplySection.tsx
+++ src/components/ApplySection.tsx
@@ -1,28 +1,38 @@
-import { ApplicationContent } from "./ApplySection.styles";
-
 type ApplySectionProps = {
-  role: string;
-  description: string;
-  statusIsOpen: boolean;
   applicationLink: string;
   closedStatus: string;
   deadline: string;
+  description: string;
+  role: string;
+  statusIsOpen: boolean;
 };
 
 const ApplySection = (props: ApplySectionProps) => (
-  <ApplicationContent data-aos="fade-down" data-aos-duration="1000">
+  <div
+    className="relative mb-[30%] rounded-[10px] bg-white p-5 text-center text-[clamp(16px,_calc(0.15vw+16px),_22px)] font-light leading-[30px] text-[rgb(20,20,20)] md:mb-[5%] md:w-[30%]"
+    data-aos="fade-down"
+    data-aos-duration="1000"
+  >
     <li>
-      <h2>{props.role}</h2>
-      <p>{props.description}</p>
+      <h2 className="text-xl font-semibold">{props.role}</h2>
+      <br />
+      <p className="mb-[10%] max-w-full sm:mb-[20%] md:mb-0 lg:mb-[15%]">
+        {props.description}
+      </p>
     </li>
     {props.statusIsOpen ? (
       <a href={props.applicationLink} rel="noreferrer" target="_blank">
-        <button className="applyPage__btn">Apply Now</button>
+        <button className="absolute inset-x-0 bottom-0 mx-[18%] mb-[-12.5%] rounded-md border-2 border-transparent bg-[var(--primary-green)] px-4 py-2 font-bold uppercase text-white shadow-md transition-transform hover:scale-[1.03] hover:border-[var(--primary-green)] hover:bg-white hover:text-[var(--primary-green)] focus:border-[var(--primary-green)] focus:bg-white focus:text-[var(--primary-green)]">
+          Apply Now
+        </button>
       </a>
     ) : (
-      <h4 className="applyPage__closed">{props.closedStatus}</h4>
+      <h4 className="absolute inset-x-0 bottom-0 mx-[18%] mb-[-12.5%] rounded-md border-2 border-[var(--primary-green)] bg-white px-4 py-2 font-bold uppercase text-[var(--primary-green)] shadow-md">
+        {props.closedStatus}
+      </h4>
     )}
-    <h5>{props.deadline}</h5>
-  </ApplicationContent>
+
+    <h5 className="text-shadow-[0.5px_0.5px_gray]">{props.deadline}</h5>
+  </div>
 );
 export default ApplySection;
